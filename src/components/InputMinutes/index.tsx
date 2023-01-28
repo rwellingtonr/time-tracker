3657da336db161a2dfda0c962e5e2c6e2f8326de
Wellington <wellington-158@hotmail.com>
2023-01-28 18:23:16 -0300

    refactor: simplificação

--- src/components/InputMinutes/index.tsx
+++
@@ -1,8 +0,0 @@
-import { InputHTMLAttributes } from "react"
-import { InputMinutesContainer } from "./styles"
-
-type InputNumberProps = InputHTMLAttributes<HTMLInputElement>
-
-export function InputNumber(props: InputNumberProps) {
-	return <InputMinutesContainer type="number" {...props} />
-}
--- src/components/InputTask/index.tsx
+++
@@ -1,8 +0,0 @@
-import { InputHTMLAttributes } from "react"
-import { InputTaskContainer } from "./styles"
-
-type InputTaskProps = InputHTMLAttributes<HTMLInputElement>
-
-export function InputTask(props: InputTaskProps) {
-	return <InputTaskContainer type="text" {...props} />
-}
