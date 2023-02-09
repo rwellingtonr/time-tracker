5827f3019579b7bd5a695db02edd4e01e1b8578c
Wellington <wellington-158@hotmail.com>
2023-02-08 22:20:15 -0300

    feat: adicionado tempo formatado

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,9 +1,25 @@
+import { useCycle } from "~/context/CyclesProvider"
 import * as Styles from "./styles"
 import { Status } from "~/components/Status/styles"
+import { Cycle } from "~/context/CyclesProvider/types"
+import { formatDistanceToNow } from "date-fns"
+import ptBR from "date-fns/locale/pt-BR"
 
 const tableHeader = ["Tarefa", "Duração", "Início", "Status"]
 
 export function History() {
+	const { cycles } = useCycle()
+
+	const renderStatus = (cycle: Cycle) => {
+		if (cycle.endTime) {
+			return <Status status="done">Concluído</Status>
+		}
+		if (cycle.interruptedTime) {
+			return <Status status="cancelled">Interrompido</Status>
+		}
+		return <Status status="ongoing">Em andamento</Status>
+	}
+
 	return (
 		<Styles.HistoryContainer>
 			<h1>Meu histórico</h1>
@@ -17,14 +33,12 @@ export function History() {
 						</tr>
 					</thead>
 					<tbody>
-						{Array.from({ length: 10 }).map((_, i) => (
-							<tr key={i}>
-								<td>{`Tarefa ${i + 1}`}</td>
-								<td>20 min</td>
-								<td>há 2 meses</td>
-								<td>
-									<Status status="ongoing">Concluído </Status>
-								</td>
+						{cycles.map((cycle) => (
+							<tr key={cycle.id}>
+								<td>{cycle.task}</td>
+								<td>{cycle.minutesAmount} minutos</td>
+								<td>{formatDistanceToNow(cycle.startTime, { addSuffix: true, locale: ptBR })}</td>
+								<td>{renderStatus(cycle)}</td>
 							</tr>
 						))}
 					</tbody>
