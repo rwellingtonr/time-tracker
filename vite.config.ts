ce2f536c060559035dbd2c216a0b8699722aa0dc
Wellington <wellington-158@hotmail.com>
2024-07-15 22:43:41 -0300

    ci: publish web page

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { resolve } from "path";
+import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: [
-			{
-				find: "~",
-				replacement: resolve(__dirname, "src"),
-			},
-		],
-	},
+	plugins: [react(), tsconfigPaths()],
+	base: '/time-tracker/'
 });
