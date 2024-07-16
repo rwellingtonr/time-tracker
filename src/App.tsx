4f0ebf9bab2d7b690760797e2d64641db4229f20
Wellington <wellington-158@hotmail.com>
2024-07-15 23:09:21 -0300

    refactor: changed to Hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/themes/default"
 import { GlobalStyle } from "./styles/globals"
 import { Router } from "./Router"
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 import { CyclesProvider } from "./context/CyclesProvider"
 
 export function App() {
 	return (
 		<ThemeProvider theme={defaultTheme}>
-			<BrowserRouter>
+			<HashRouter>
 				<CyclesProvider>
 					<Router />
 				</CyclesProvider>
-			</BrowserRouter>
+			</HashRouter>
 			<GlobalStyle />
 		</ThemeProvider>
 	)
